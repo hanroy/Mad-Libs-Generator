a12e280fe43ccf0295f2713956727b392ffc9b6e
hanroy <royer.hanen@gmail.com>
2017-05-19 00:04:07 +0200

    Add my mad lib code

---
+++ mad-libs.py
@@ -0,0 +1,19 @@
+noun1 = raw_input("Enter the name of your crush, and click enter: ")
+adjective1 = raw_input("Enter an adjective, and click enter: ")
+verb = raw_input("Enter a verb present tense, and click enter: ")
+body_part1 = raw_input("Enter a body part, and click enter: ")
+number = raw_input("Enter a number, and click enter: ")
+noun2 = raw_input("Enter a noun, and click enter: ")
+adverb = raw_input("Enter an adverb, and click enter: ")
+verb2 = raw_input("Enter another verb, and click enter: ")
+plural_noun = raw_input("Enter a plural noun, and click enter: ")
+noum3 = raw_input("Enter your name, and click enter: ")
+
+print("\n\nARE YOU READY, Here is your letter!")
+
+print("\n\nDear {},\n\n"
+"You are extremly {} and I {} you! I want kiss your {} {} times. "
+"You make my {} burn with desire.\n"
+"When I first saw you, I {} stared at you and fell in love. "
+"Will you {} out with me? Don`t let your parents discourage you, {} are just jealous.\n\n"
+"Yours forever, {}".format(noun1, adjective1, verb, body_part1, number, noun2, adverb, verb2, plural_noun, noum3))
